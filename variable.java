fba80656cb466eee92fa75df6c55839ee7712886
Sir Willys <whurtadob@tecnocomfenalco.edu.co>
2024-12-17 13:33:16 -0500

    first commit

---
+++ variable.java
@@ -0,0 +1,23 @@
+public class variable{
+
+    public static void main(String[] args) {
+        
+
+        int number = 30;
+        String texto = "hola este es un texto";
+        boolean bolean= true;
+        double doubles = 34.90;
+        float flotante= 3444.0f;
+        
+
+
+
+
+
+    }
+
+
+
+
+
+}
